00d691913122e0b7ac202a5e48c7021876b671cf
shafe <shafe@6280e2b0-3b14-0410-9080-8d9db04c143e>
2006-09-26 04:20:36 +0000

    - Shafe - error messages for sounds not found are now displayed from cgame and ui code instead of the engine.

--- iotrep-engine/code/client/snd_dma.c
+++ iotrep-engine/code/client/snd_dma.c
@@ -329,7 +329,8 @@ sfxHandle_t	S_Base_RegisterSound( const char *name, qboolean compressed ) {
 	sfx = S_FindName( name );
 	if ( sfx->soundData ) {
 		if ( sfx->defaultSound ) {
-			Com_Printf( S_COLOR_YELLOW "WARNING: could not find %s - using default\n", sfx->soundName );
+			// Shafe - Let's check this outside of the engine
+			//Com_Printf( S_COLOR_YELLOW "WARNING: could not find %s - using default\n", sfx->soundName );
 			return 0;
 		}
 		return sfx - s_knownSfx;
@@ -341,7 +342,8 @@ sfxHandle_t	S_Base_RegisterSound( const char *name, qboolean compressed ) {
   S_memoryLoad(sfx);
 
 	if ( sfx->defaultSound ) {
-		Com_Printf( S_COLOR_YELLOW "WARNING: could not find %s - using default\n", sfx->soundName );
+		// Shafe - Let's check this outside of the engine
+		// Com_Printf( S_COLOR_YELLOW "WARNING: could not find %s - using default\n", sfx->soundName );
 		return 0;
 	}
 
