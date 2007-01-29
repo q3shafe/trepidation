d1341fc7466c6ee891cd67b1f6d1e8c8bf844856
shafe <q3shafe@users.noreply.github.com>
2007-01-29 10:14:36 +0000

    - Bots will now try to go after buildables... thus making them able to play trep gametype.. They dont build defensive structures, but at least you arent screwed if your stuck on a team with a bot.

--- vms-source/code/game/ai_dmq3.c
+++ vms-source/code/game/ai_dmq3.c
@@ -3022,12 +3022,15 @@ BotFindEnemy
 ==================
 */
 int BotFindEnemy(bot_state_t *bs, int curenemy) {
-	int i, healthdecrease;
+	int i, healthdecrease, j;
 	float f, alertness, easyfragger, vis;
 	float squaredist, cursquaredist;
 	aas_entityinfo_t entinfo, curenemyinfo;
 	vec3_t dir, angles;
 	// Trep
+		gentity_t	*e, *e2;
+		int		c, c2;
+
 //	int ent;
 //	entityState_t state;
 	
@@ -3050,35 +3053,8 @@ int BotFindEnemy(bot_state_t *bs, int curenemy) {
 	}
 
 	
-	/*
-	// Trepidation Gametype
-	// This didnt work at all
-	if (g_GameMode.integer == 3 && curenemy == 0)
-	{
-		
-		ent = 0;
-		i=0;
-		while( ( ent = BotAI_GetSnapshotEntity( bs->client, ent, &state ) ) != -1 ) {
-		
-			
-			if (state.eType == ET_BUILDABLE)
-			{
-				entinfo.number = ent;
-				//BotEntityInfo(i, &entinfo);
-				bs->enemy = entinfo.number;
-				if (curenemy >= 0) bs->enemysight_time = FloatTime() - 2;
-				else bs->enemysight_time = FloatTime();
-				bs->enemysuicide = qfalse;
-				bs->enemydeath_time = 0;
-				bs->enemyvisible_time = FloatTime();
-				return qtrue;
-			}
 
 
-		}
-
-	}
-	*/
 
 #ifdef MISSIONPACK
 	if (gametype == GT_OBELISK) {
@@ -3178,6 +3154,111 @@ int BotFindEnemy(bot_state_t *bs, int curenemy) {
 		bs->enemyvisible_time = FloatTime();
 		return qtrue;
 	}
+
+	// Trepidation Gametype - An enemy is also a buildable
+	if (g_GameMode.integer == 3 && curenemy == 0)
+	{
+		c = 0;
+		c2 = 0;
+		for ( i=1, e=g_entities+i ; i < level.num_entities ; i++,e++ )
+		{
+
+			if (e->s.eType == ET_BUILDABLE && e->s.team != BotTeam(bs))
+			{
+				
+				
+				if (i == curenemy) continue;
+				//
+				BotEntityInfo(i, &entinfo);
+				//
+				if (!entinfo.valid) continue;
+				//if the enemy isn't dead and the enemy isn't the bot self
+				if (EntityIsDead(&entinfo) || entinfo.number == bs->entitynum) continue;
+				//if the enemy is invisible and not shooting
+				if (EntityIsInvisible(&entinfo) && !EntityIsShooting(&entinfo)) {
+					continue;
+				}
+		//unlagged - misc
+				// this has nothing to do with lag compensation, but it's great for testing
+				if ( g_entities[i].flags & FL_NOTARGET ) continue;
+		//unlagged - misc
+				//if not an easy fragger don't shoot at chatting players
+				if (easyfragger < 0.5 && EntityIsChatting(&entinfo)) continue;
+				//
+				if (lastteleport_time > FloatTime() - 3) {
+					VectorSubtract(entinfo.origin, lastteleport_origin, dir);
+					if (VectorLengthSquared(dir) < Square(70)) continue;
+				}
+				//calculate the distance towards the enemy
+				VectorSubtract(entinfo.origin, bs->origin, dir);
+				squaredist = VectorLengthSquared(dir);
+				//if this entity is not carrying a flag
+				if (!EntityCarriesFlag(&entinfo))
+				{
+					//if this enemy is further away than the current one
+					if (curenemy >= 0 && squaredist > cursquaredist) continue;
+				} //end if
+				//if the bot has no
+				if (squaredist > Square(900.0 + alertness * 4000.0)) continue;
+				//if on the same team
+				//if (BotSameTeam(bs, i)) continue;
+				//if the bot's health decreased or the enemy is shooting
+				if (curenemy < 0 && (healthdecrease || EntityIsShooting(&entinfo)))
+					f = 360;
+				else
+					f = 90 + 90 - (90 - (squaredist > Square(810) ? Square(810) : squaredist) / (810 * 9));
+				//check if the enemy is visible
+				vis = BotEntityVisible(bs->entitynum, bs->eye, bs->viewangles, f, i);
+				if (vis <= 0) continue;
+				//if the enemy is quite far away, not shooting and the bot is not damaged
+				if (curenemy < 0 && squaredist > Square(100) && !healthdecrease && !EntityIsShooting(&entinfo))
+				{
+					//check if we can avoid this enemy
+					VectorSubtract(bs->origin, entinfo.origin, dir);
+					vectoangles(dir, angles);
+					//if the bot isn't in the fov of the enemy
+					if (!InFieldOfVision(entinfo.angles, 90, angles)) {
+						//update some stuff for this enemy
+						BotUpdateBattleInventory(bs, i);
+						//if the bot doesn't really want to fight
+						if (BotWantsToRetreat(bs)) continue;
+					}
+				}
+				//found an enemy
+				bs->enemy = entinfo.number;
+				if (curenemy >= 0) bs->enemysight_time = FloatTime() - 2;
+				else bs->enemysight_time = FloatTime();
+				bs->enemysuicide = qfalse;
+				bs->enemydeath_time = 0;
+				bs->enemyvisible_time = FloatTime();
+				return qtrue;
+				
+
+
+
+				/*
+				entinfo.number = i;
+				BotEntityInfo(i, &entinfo);
+				bs->enemy = entinfo.number;
+				if (curenemy >= 0) bs->enemysight_time = FloatTime() - 2;
+				else bs->enemysight_time = FloatTime();
+				bs->enemysuicide = qfalse;
+				bs->enemydeath_time = 0;
+				bs->enemyvisible_time = FloatTime();
+				return qtrue;
+				*/
+
+			}
+
+			c++;
+			c2++;	
+		
+		}
+
+	}
+
+
+
 	return qfalse;
 }
 
@@ -4285,8 +4366,6 @@ BotGetActivateGoal
 
   returns the number of the bsp entity to activate
   goal->entitynum will be set to the game entity to activate
-
-  // FIXME: This needs cleaned up - Shafe
 ==================
 */
 //#define OBSTACLEDEBUG
@@ -4326,87 +4405,12 @@ int BotGetActivateGoal(bot_state_t *bs, int entitynum, bot_activategoal_t *activ
 				return ent;
 			}
 		}
-	}
 
-	/* This doesnt go here
-
-
-	// Trepidation - Bot shoots at buildabled
-
-	//if it is a Turret
-	if (!strcmp(classname, "turret")) {
-		if (trap_AAS_FloatForBSPEpairKey(ent, "health", &health)) {
-			//if the door has health then the door must be shot to open
-			if (health) {
-				BotFuncDoorActivateGoal(bs, ent, activategoal);
-				return ent;
-			}
-		}
-	}
-
-	//if it is a generator
-	if (!strcmp(classname, "turret")) {
-		if (trap_AAS_FloatForBSPEpairKey(ent, "health", &health)) {
-			//if the door has health then the door must be shot to open
-			if (health) {
-				BotFuncDoorActivateGoal(bs, ent, activategoal);
-				return ent;
-			}
-		}
-	}
-
-	//if it is an immobilizer
-	if (!strcmp(classname, "timedisplacer")) {
-		if (trap_AAS_FloatForBSPEpairKey(ent, "health", &health)) {
-			//if the door has health then the door must be shot to open
-			if (health) {
-				BotFuncDoorActivateGoal(bs, ent, activategoal);
-				return ent;
-			}
-		}
-
-	//if it is a buildable
-	if (!strcmp(classname, "mc")) {
-		if (trap_AAS_FloatForBSPEpairKey(ent, "health", &health)) {
-			//if the door has health then the door must be shot to open
-			if (health) {
-				BotFuncDoorActivateGoal(bs, ent, activategoal);
-				return ent;
-			}
-		}
-	}
-
-	// Someone's PDG Grenade
-	//if it is a buildable
-	if (!strcmp(classname, "pdg")) {
-		if (trap_AAS_FloatForBSPEpairKey(ent, "health", &health)) {
-			//if the door has health then the door must be shot to open
-			if (health) {
-				BotFuncDoorActivateGoal(bs, ent, activategoal);
-				return ent;
-			}
-		}
-	}
-
-   */
-
-	//if it is a door
-	if (!strcmp(classname, "func_breakable")) {
-		if (trap_AAS_FloatForBSPEpairKey(ent, "health", &health)) {
-			//if the door has health then the door must be shot to open
-			if (health) {
-				BotFuncDoorActivateGoal(bs, ent, activategoal);
-				return ent;
-			}
-		}
-	}
-
-	/*
 	//if it is some glass
  	if (!strcmp(classname, "func_breakable")) {
  		return ent;
  	}
-	*/
+	
 		//
 		trap_AAS_IntForBSPEpairKey(ent, "spawnflags", &spawnflags);
 		// if the door starts open then just wait for the door to return
