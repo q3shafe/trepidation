0f2c8104d0135d041c6bac960a0d8eac3d2e05b6
shafe <shafe@6280e2b0-3b14-0410-9080-8d9db04c143e>
2011-11-29 05:44:42 +0000

    - Updated CHANGELOG.TXT - Added Linux Distribution Files - Added ded-server.bat asset for windows installer

---
+++ Docs/ded-server.bat
@@ -0,0 +1,1 @@
+trepidation +set dedicated 2 +exec trepidation.cfg +set net_port 23060
---
+++ Docs/linux/dedicated_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/trepidation
+./trepded.i386 +set net_ip YOURIP +set +set com_hunkmegs 120 +set dedicated 2 +exec trepidation.cfg > /dev/null 2>&1 &
+
