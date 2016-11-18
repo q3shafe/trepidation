a9654cfd07f7cc68fbf40a18b875d2f01e59e44c
Shafe <q3shafe@gmail.com>
2016-11-18 00:31:39 -0500

    - changed binary filenames for consistancy. - Updated windows server startup script.

--- Docs/ded-server.bat
+++ Docs/ded-server.bat
@@ -1,1 +1,2 @@
-trepidation-dedicated +set dedicated 2 +exec trep-server.cfg +set net_port 23060
+trepded.i386.exe +set net_ip=127.0.0.1 +set dedicated 2 +exec trep-server.cfg +set net_port 23060
+rem Replace 127.0.0.1 with your IP Address.
