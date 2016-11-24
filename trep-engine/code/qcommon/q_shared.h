3a4620e2f8cce7eae562a37055256bef8cdc749f
Shafe <q3shafe@gmail.com>
2016-11-24 16:30:46 -0500

    - Updated version info

--- trep-engine/code/qcommon/q_shared.h
+++ trep-engine/code/qcommon/q_shared.h
@@ -34,7 +34,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   #define GAMENAME_FOR_MASTER		"Trepidation"		// must NOT contain whitespaces
   #define HEARTBEAT_FOR_MASTER		"Trepidation-1"
 #else
-  #define PRODUCT_NAME			"Trepidation SNAPSHOT"
+  #define PRODUCT_NAME			"Trepidation"
   #define BASEGAME			"base"
   #define CLIENT_WINDOW_TITLE     	"Trepidation Console"
   #define CLIENT_WINDOW_MIN_TITLE 	"Trepidation Console"
@@ -43,7 +43,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 #ifdef _MSC_VER
-  #define PRODUCT_VERSION "0.10"
+  #define PRODUCT_VERSION "11.24.2016"
 #endif
 
 #define Q3_VERSION PRODUCT_NAME "Trepidation" PRODUCT_VERSION
