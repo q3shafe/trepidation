d6272df23b1b89940d30f5b8122e1ee223ff55b0
Shafe <q3shafe@gmail.com>
2016-11-24 17:45:05 -0500

    updated game build info

--- vms-source/code/game/g_client.c
+++ vms-source/code/game/g_client.c
@@ -1028,7 +1028,7 @@ char *ClientConnect( int clientNum, qboolean firstTime, qboolean isBot ) {
 
 //unlagged - backward reconciliation #5
 	// announce it
-	trap_SendServerCommand( clientNum, "print \"Trepidation Development SNAPSHOT\n\"" );
+	trap_SendServerCommand( clientNum, "print \"Trepidation Development Build\n\"" );
 	if ( g_delagHitscan.integer ) {
 //		trap_SendServerCommand( clientNum, "print \"This server is Unlagged: full lag compensation is ON!\n\"" );
 	}
