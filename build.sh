79fd371b065218b13489d2d381f16e65f19e181c
Shafe <q3shafe@gmail.com>
2016-11-29 14:03:22 -0500

    - updated nightly snapshot build script to include macosx binaries

--- build.sh
+++ build.sh
@@ -1,35 +1,46 @@
-echo "Build from github\n"
-echo "This will build you a working snapshot of Trepidation\n";
-echo "The working game directory will be trep/build\n";
-mkdir trep
-cd trep
-mkdir build
-mkdir build/base
-mkdir build/docs
-wget https://ioquake3.org/files/xcsv_hires.zip
+mkdir /home/trep/www/build
+mkdir /home/trep/www/build/base
+mkdir /home/trep/www/build/docs
+
+wget http://ioquake3.org/files/xcsv_hires.zip
 unzip xcsv_hires.zip
-cp *.pk3 build/base
+cp *.pk3 /home/trep/www/build/base
+cd /home/trep
 git clone https://github.com/q3shafe/trepidation.git
 cd trepidation/game-media
-cp *.dll ../../build
-cp trep* ../../build
+cp *.dll /home/trep/www/build
+cp trep* /home/trep/www/build
 cd pak0-gpl2data
-zip -r ../../../build/base/pak0-gpl2data.pk3 *
+zip -r /home/trep/www/build/base/pak0-gpl2data.pk3 *
 cd ..
 cd pak1-ccdata
-zip -r ../../../build/base/pak1-ccdata.pk3 *
+zip -r /home/trep/www/build/base/pak1-ccdata.pk3 *
 cd ..
 cd pak0-vms
-zip -r ../../../build/base/pak0-vms.pk3 *
-cd ../../../
-cp trepidation/Docs/server/* build/base
-cp trepidation/Docs/linux/*.sh build/
-cp trepidation/Docs/linux/R* build/docs/
-cp trepidation/Docs/quake3/* build/docs/
-cp trepidation/Docs/*.txt build/docs/
-cp trepidation/Docs/*.TXT build/docs/
-cp trepidation/Docs/*.bat build/
-cd build
-#zip -r trep-snapshot.zip *
-
-
+zip -r /home/trep/www/build/base/pak0-vms.pk3 *
+mkdir /home/trep/www/build/macosx
+cp -R /home/trep/trepidation/game-media/macosx/* /home/trep/www/build
+cp /home/trep/trepidation/Docs/server/* /home/trep/www/build/base
+cp /home/trep/trepidation/Docs/linux/*.sh /home/trep/www/build
+cp /home/trep/trepidation/Docs/linux/R* /home/trep/www/docs
+cp /home/trep/trepidation/Docs/quake3/* /home/trep/www/build/docs
+cp /home/trep/trepidation/Docs/*.txt /home/trep/www/build/docs
+cp /home/trep/trepidation/Docs/*.TXT /home/trep/www/build/docs
+cp /home/trep/trepidation/Docs/*.bat /home/trep/www/build
+cp /home/trep/trepidation/Docs/*.html /home/trep/www/build/docs
+cd /home/trep/www/build
+zip -r trep-snapshot.zip *
+cp *.zip /home/trep
+cd /home/trep/trepidation
+rm -rf *
+rm -rf .g*
+cd /home/trep
+rmdir trepidation
+cd /home/trep/www/build
+rm -rf *
+cd /home/trep
+cp trep-snapshot.zip /home/sites/1601/www/dls
+rm trep-snapshot.zip
+rm *.zip
+rm *.pk3
+rm *.txt
