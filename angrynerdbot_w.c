f9a8a83b4abe39eb133ba80d272ddaf41d908527
angrynerd <angrynerd2103@gmail.com>
2017-05-26 15:07:54 -0400

    Add files via upload

---
+++ angrynerdbot_w.c
@@ -0,0 +1,17 @@
+
+
+#include "inv.h"
+
+#define W_GAUNTLET				10
+#define W_SHOTGUN				5
+#define W_MACHINEGUN			100
+#define W_GRENADELAUNCHER		150
+#define W_ROCKETLAUNCHER		75
+#define W_RAILGUN				50
+#define W_BFG10K				25
+#define W_LIGHTNING				200
+#define W_PLASMAGUN				200
+#define W_GRAPPLE				15
+
+//
+#include "fw_weap.c"
