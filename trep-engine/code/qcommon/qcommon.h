fbbb3e1a03f4714a98fb3fb0bf2ca46b418261d1
Shafe <q3shafe@gmail.com>
2017-08-28 22:59:11 -0400

    - Updated master protocol

--- trep-engine/code/qcommon/qcommon.h
+++ trep-engine/code/qcommon/qcommon.h
@@ -242,7 +242,7 @@ PROTOCOL
 ==============================================================
 */
 
-#define	PROTOCOL_VERSION	68
+#define	PROTOCOL_VERSION	69
 // 1.31 - 67  -- Shafe - Previous 68
 
 // maintain a list of compatible protocols for demo playing
