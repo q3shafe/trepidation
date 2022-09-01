336932dc0b62eba4a88b2c0df56e3524335f0417
q3shafe <q3shafe@gmail.com>
2022-09-01 06:30:58 -0400

    - Updated website address - Updated master server addresses

--- vms-source/code/q3_ui/ui_credits.c
+++ vms-source/code/q3_ui/ui_credits.c
@@ -49,7 +49,7 @@ typedef struct
 cr_line credits[] = { // edit this as necessary for your credits
 	
 { "Trepidation", UI_CENTER|UI_BIGFONT|UI_PULSE, &color_headertext },
-{ "planettrepidation.tk", UI_CENTER|UI_SMALLFONT, &color_headertext },
+{ "trepidation.n5net.com", UI_CENTER|UI_SMALLFONT, &color_headertext },
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 
 { "Development Team:", UI_CENTER|UI_BIGFONT|UI_PULSE, &color_headertext },
@@ -139,7 +139,7 @@ cr_line credits[] = { // edit this as necessary for your credits
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 
 { "Visit Us On The Web @", UI_CENTER|UI_SMALLFONT, &color_headertext },
-{ "planettrepidation.tk", UI_CENTER|UI_SMALLFONT, &color_maintext },
+{ "trepidation.n5net.com", UI_CENTER|UI_SMALLFONT, &color_maintext },
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 
