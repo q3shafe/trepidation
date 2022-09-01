a2d9fc3cffda6ac442368f157224716ad6b264c2
q3shafe <q3shafe@gmail.com>
2022-09-01 05:50:06 -0400

    - Updating version for next build

--- trep-engine/code/qcommon/q_shared.h
+++ trep-engine/code/qcommon/q_shared.h
@@ -43,7 +43,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 #ifdef _MSC_VER
-  #define PRODUCT_VERSION "v0.0.27"
+  #define PRODUCT_VERSION "DevBuild"
 #endif
 
 #define Q3_VERSION PRODUCT_NAME " " PRODUCT_VERSION
