f0caa98eece03c299e6478ab2fd5d27772fb6a71
q3shafe <q3shafe@gmail.com>
2023-03-03 03:31:46 -0500

    - Website updates

--- website/www/index.php
+++ website/www/index.php
@@ -17,7 +17,7 @@
 	<header>
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container mx-auto">
-				<a class="navbar-brand" href="#">Trepidation FPS</a>
+				<a class="navbar-brand" href="/">Trepidation FPS</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
