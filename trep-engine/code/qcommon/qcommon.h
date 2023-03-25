bf4e3dfe7f3e055846e36f4c246bcf1da8283ccf
q3shafe <q3shafe@gmail.com>
2023-03-25 01:42:08 -0400

    - Missed files from previus commig

--- trep-engine/code/qcommon/qcommon.h
+++ trep-engine/code/qcommon/qcommon.h
@@ -253,9 +253,9 @@ extern int demo_protocols[];
 // override on command line, config files etc.
 #ifndef MASTER_SERVER_NAME
 #define MASTER_SERVER_NAME	"master.n5net.com" // Shafe - Trep 
-#define ALT_MASTER0	"master.n5net.com" // Shafe - Trep
+#define ALT_MASTER0	"trepidationfps.com" // Shafe - Trep
 #define ALT_MASTER1	"server1.u7radio.org" // Shafe - Trep
-#define ALT_MASTER2	"master2.n5net.com" // Shafe - Trep
+#define ALT_MASTER2	"server2.n5net.com" // Shafe - Trep
 #define ALT_MASTER3	"trepidation.n5net.com" // Shafe - Trep -
 #endif
 
--- vms-source/code/q3_ui/ui_credits.c
+++ vms-source/code/q3_ui/ui_credits.c
@@ -139,14 +139,14 @@ cr_line credits[] = { // edit this as necessary for your credits
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 
 { "Visit Us On The Web @", UI_CENTER|UI_SMALLFONT, &color_headertext },
-{ "trepidationfps.com / trepidation.n5net.com", UI_CENTER|UI_SMALLFONT, &color_maintext },
+{ "trepidationfps.com / trepidationfps.com", UI_CENTER|UI_SMALLFONT, &color_maintext },
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 
 
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
 { "", UI_CENTER|UI_SMALLFONT, &color_blue },
-{ "(c)2006-2023 Team Trepidation / N5 Networks", UI_CENTER|UI_SMALLFONT, &color_maintext },
+{ "(c)2006-2023 Team Trepidation", UI_CENTER|UI_SMALLFONT, &color_maintext },
 //{ "General Public License", UI_CENTER|UI_SMALLFONT, &color_maintext },
 
   {NULL}
