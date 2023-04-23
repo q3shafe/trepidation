9fa33d19b79297d2398496ff56c6483e33792699
q3shafe <q3shafe@gmail.com>
2023-04-23 04:55:35 -0400

    Revert "- Fixed Ded server startup files"
    This reverts commit b86a763132273cbd83d77ba3c4e32ccd3b253a38.
    
    # Conflicts:
    #	Docs/CHANGELOG.TXT
    #	game-media/pak0-vms/vm/qagame.qvm
    #	game-media/pak0-vms/vm/ui.qvm

--- Docs/ded-server.bat
+++ Docs/ded-server.bat
@@ -1,2 +1,2 @@
-trepded.x86.exe +set  net_ip=127.0.0.1 +set com_hunkmegs 120 +set dedicated 2 +exec trep-server.cfg 
+trepded.x86.exe +set  net_ip=108.161.128.117 +set com_hunkmegs 120 +set dedicated 2 +exec trep-server.cfg 
 rem Replace 127.0.0.1 with your IP Address.
